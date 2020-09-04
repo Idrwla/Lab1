eae5ebf1039d90584b352e21bc21c4ce64eefb16
Idrwla <ilyarredmi@gmail.com>
2020-09-04 23:35:24 +0600

    застрял на выводе данных на таблицу

---
+++ main.js
@@ -0,0 +1,67 @@
+var regexEmail = /^[a-zA-Z0-9._@]+$/
+var regextwo = /@/
+var regexrating = /^[1-5]/
+var usercomment = {
+    email :"",
+    comment:"",
+    rating:"",
+    date:Date()
+
+};
+
+function Addrow(){
+    var table =document.getElementsByTagName("table")[0];
+    var trow = table.insertRow(1);
+    var cel0 = trow.insertCell(0);
+    var cel1 = trow.insertCell(1);
+    var cel2 = trow.insertCell(2);
+    var cel3 = trow.insertCell(2);
+    var cel4 = trow.insertCell(4);
+    cel0.innerHTML =document.getElementById("curri")+1;
+    cel1.innerHTML = usercomment.email;
+    cel2.innerHTML = usercomment.comment;
+    cel3.innerHTML=usercomment.date;
+    cel4.innerHTML=usercomment.rating;
+    return table;
+}
+function Valideete(){
+    var x = document.forms["mainform"]["email"].value;
+    var y = document.forms["mainform"]["comment"].value;
+    var z = document.forms["mainform"]["level"].value;
+    if(!(regexEmail.test(x) && regextwo.test(x))){
+        document.getElementById("erremail").innerHTML ="Fill the e mail areaлибо проверьте правильность ввода ";
+        return false;
+    }else{
+        document.getElementById("erremail").innerHTML ="";
+    }
+    if(y.length<=3 || y.length>100){
+        document.getElementById("errtext").innerHTML = "Write the comment comment must be at least 3 chars and maximum 100";
+        return false;
+    }else{
+        document.getElementById("errtext").innerHTML = "";
+    }
+    if(!(regexrating.test(z))){
+        document.getElementById("errradio").innerHTML="Choose the comment Level!";
+        return false;
+    }else{
+        document.getElementById("errradio").innerHTML="";
+    }
+    usercomment.email = x;
+    usercomment.comment=y;
+    usercomment.rating = z;
+    usercomment.date=new Date();
+    /*var table =document.getElementsByTagName("table")[0];
+    var trow = table.insertRow(1);
+    var cel0 = trow.insertCell(0);
+    var cel1 = trow.insertCell(1);
+    var cel2 = trow.insertCell(2);
+    var cel3 = trow.insertCell(2);
+    var cel4 = trow.insertCell(4);
+    cel0.innerHTML =document.getElementById("curri")+1;
+    cel1.innerHTML = usercomment.email;
+    cel2.innerHTML = usercomment.comment;
+    cel3.innerHTML=usercomment.date;
+    cel4.innerHTML=usercomment.rating;*/
+    return Addrow();
+}
+
