3306b598882f59b6f258f805d7679e548c092188
Idrwla <ilyarredmi@gmail.com>
2020-09-05 18:33:41 +0600

    var заменен на let

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ var usercomment = {
 };
 
 function Addrow(){
-    var table =document.getElementsByTagName("table")[0];
-    var trow = table.insertRow(1);
-    var cel0 = trow.insertCell(0);
-    var cel1 = trow.insertCell(1);
-    var cel2 = trow.insertCell(2);
-    var cel3 = trow.insertCell(3);
-    var cel4 = trow.insertCell(4);
-    var n  = parseInt(document.getElementById("curri").innerHTML);
+    let table =document.getElementsByTagName("table")[0];
+    let trow = table.insertRow(1);
+    let cel0 = trow.insertCell(0);
+    let cel1 = trow.insertCell(1);
+    let cel2 = trow.insertCell(2);
+    let cel3 = trow.insertCell(3);
+    let cel4 = trow.insertCell(4);
+    let n  = parseInt(document.getElementById("curri").innerHTML);
     cel0.innerHTML =n+1;
     cel1.innerHTML = usercomment.email;
     cel2.innerHTML = usercomment.comment;
@@ -27,40 +27,40 @@ function Addrow(){
 }
 
 function AddrowDOM() {
-    var table1 = document.getElementById("table");
-    var row = document.createElement("tr");
-    var data1 = document.createElement("td");
-    var data2 = document.createElement("td");
-    var data3 = document.createElement("td");
-    var data4 = document.createElement("td");
-    var data5 = document.createElement("td");
+    let table = document.getElementById("table");
+    let row = document.createElement("tr");
+    let data1 = document.createElement("td");
+    let data2 = document.createElement("td");
+    let data3 = document.createElement("td");
+    let data4 = document.createElement("td");
+    let data5 = document.createElement("td");
     row.appendChild(data1);
     row.appendChild(data2);
     row.appendChild(data3);
     row.appendChild(data4);
     row.appendChild(data5);
-    node1 = document.createTextNode(parseInt(document.getElementById("curri").innerHTML)+1);
-    node2=document.createTextNode(usercomment.email);
-    node3 = document.createTextNode(usercomment.comment);
-    node4 = document.createTextNode(usercomment.date.getDate()+"/"+usercomment.date.getMonth()+"/"+usercomment.date.getFullYear() + "  "+ usercomment.date.getHours() +":"+usercomment.date.getMinutes());
-    node5 = document.createTextNode(usercomment.rating);
+    let node1 = document.createTextNode(parseInt(document.getElementById("curri").innerHTML)+1);
+    let node2=document.createTextNode(usercomment.email);
+    let node3 = document.createTextNode(usercomment.comment);
+    let node4 = document.createTextNode(usercomment.date.getDate()+"/"+usercomment.date.getMonth()+"/"+usercomment.date.getFullYear() + "  "+ usercomment.date.getHours() +":"+usercomment.date.getMinutes());
+    let node5 = document.createTextNode(usercomment.rating);
     data1.appendChild(node1);
     data2.appendChild(node2);
     data3.appendChild(node3);
     data4.appendChild(node4);
     data5.appendChild(node5);
-    table1.appendChild(row);
+    table.appendChild(row);
 
 
 
 
-return table1;
+return table;
 
 }
 function Valideete(){
-    var x = document.forms["mainform"]["email"].value;
-    var y = document.forms["mainform"]["comment"].value;
-    var z = document.forms["mainform"]["level"].value;
+    let x = document.forms["mainform"]["email"].value;
+    let y = document.forms["mainform"]["comment"].value;
+    let z = document.forms["mainform"]["level"].value;
     if(!(regexEmail.test(x) && regextwo.test(x))){
         document.getElementById("erremail").innerHTML ="Fill the e mail areaлибо проверьте правильность ввода ";
         return false;
